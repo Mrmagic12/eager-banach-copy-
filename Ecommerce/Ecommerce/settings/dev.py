94d00f0fd4d42fee9eb949e907a740609d2d3add
Mrmagic12 <157940462+Mrmagic12@users.noreply.github.com>
2024-04-06 23:57:50 +0000

    added new project

---
+++ Ecommerce/Ecommerce/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-zkfe@oifea)1rg95xt=u2wcbnvrh)n=g@go7g9ldo5z*wy36vp"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
